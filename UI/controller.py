08bf511c72b5dffb5da648cb42379c6e045d8ae0
jacopo <jacopo.carmelini@gmail.com>
2025-11-27 23:24:24 +0100

    completo

--- UI/controller.py
+++ UI/controller.py
@@ -26,23 +26,27 @@ class Controller:
         try:
             minimo = float(treshold)
             self._model.costruisci_grafo(minimo)
+            num_hub = self._model.get_num_nodes()
+            num_tratte = self._model.get_num_edges()
+            tratte = self._model.get_all_edges()
+
+            self._view.lista_visualizzazione.controls.clear()
+            self._view.lista_visualizzazione.controls.append(ft.Text(f"Numero di hub: {num_hub}"))
+            self._view.lista_visualizzazione.controls.append(ft.Text(f"Numero di tratte: {num_tratte}"))
+
+            counter = 0
+            for u, v, peso in tratte:
+                counter += 1
+                self._view.lista_visualizzazione.controls.append(
+                    ft.Text(f"{counter}) {u.nome} ({u.stato}) -> {v.nome} ({v.stato}) -- Guadagno medio per spedizione € {peso:.2f}")
+                )
+            self._view.update()
 
         except ValueError:
             self._view.show_alert("Inserisci guadagno minimo valido")
             return
 
-        num_hub = self._model.get_num_nodes()
-        num_tratte = self._model.get_num_edges()
-        tratte = self._model.get_all_edges()
 
-        self._view.lista_visualizzazione.controls.clear()
-        self._view.lista_visualizzazione.controls.append(ft.Text(f"Numero di hub: {num_hub}"))
-        self._view.lista_visualizzazione.controls.append(ft.Text(f"Numero di tratte: {num_tratte}"))
-
-        for u, v, peso in tratte:
-            self._view.lista_visualizzazione.controls.append(
-                ft.Text(f"{u.nome}({u.stato} -> {v.nome}({v.stato} -- Guadagno medio per spedizione {}")
-            )
 
 
 
